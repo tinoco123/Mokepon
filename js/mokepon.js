922d106426ea763c3796432853807e4adc3472ef
Jonathan Tinoco <tinocojona@outlook.com>
2023-01-10 23:48:01 -0600

    ocultar elementos html 4 ux

--- js/mokepon.js
+++ js/mokepon.js
@@ -16,6 +16,11 @@ function iniciarJuego(){
     botonAgua.addEventListener("click", ataqueAgua)
     botonTierra.addEventListener("click", ataqueTierra)
     botonReiniciar.addEventListener("click", reiniciarJuego)
+    // Desactivar contenido para evitar un mal flujo del juego
+    let seccionAtaques = document.getElementById("seleccionar-ataque")
+    seccionAtaques.style.display = "none"
+    let seccionReiniciar = document.getElementById("reiniciar")
+    seccionReiniciar.style.display = "none"
 }
 
 
@@ -37,22 +42,59 @@ function ataqueTierra(){
 }
 
 
-function crearMensajeCombate(){
-    let mensajes = document.getElementById("mensajes")
-
-    let parrafo =  document.createElement("p")
-    parrafo.innerHTML = `Tu mascota atacó con ${ataqueJugador}, la mascota del enemigo atacó con ${ataqueEnemigo} - ${resultado}`
-
-    mensajes.appendChild(parrafo)
+function seleccionarAtaqueEnemigo(){
+    let ataques = new Map();
+    ataques.set(1, "FUEGO")
+    ataques.set(2, "AGUA")
+    ataques.set(3, "TIERRA")
+    ataqueEnemigo = ataques.get(numeroAleatorio(1,3))
+    combate()
+    crearMensajeCombate()
+    if (vidasJugador == 0 || vidasEnemigo == 0) {
+        deshabilitarAtaques()
+        crearMensajeFinal()
+        let seccionReiniciar = document.getElementById("reiniciar")
+        seccionReiniciar.style.display = "block"
+    }
 }
 
-function crearMensajeFinal(){
-    let mensajes = document.getElementById("mensajes")
 
-    let parrafo =  document.createElement("p")
-    parrafo.innerHTML = ` ${vidasJugador == 0 ? "Losiento, has perdido:(" : "Felicidades has ganado"}`
+function seleccionarMascotaJugador(){
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+    let mascotaJugador = document.getElementById("mascota-jugador")
 
-    mensajes.appendChild(parrafo)
+    if (inputHipodoge.checked){
+        alert("Seleccionaste la mascota Hipodoge")
+        mascotaJugador.innerHTML = "Hipodoge"
+    } else if (inputCapipepo.checked){
+        alert("Seleccionaste la mascota Capipepo")
+        mascotaJugador.innerHTML = "Capipepo"
+    } else if (inputRatigueya.checked){
+        alert("Seleccionaste la mascota Ratigueya")
+        mascotaJugador.innerHTML = "Ratigueya"
+    } else{
+        alert("No seleccionaste ninguna mascota")
+    }
+    if (mascotaJugador.innerHTML != ""){
+        seleccionarMascotaEnemigo()
+        let seccionAtaques = document.getElementById("seleccionar-ataque")
+        seccionAtaques.style.display = "block"
+        let seccionMascota = document.getElementById("seleccionar-mascota")
+        seccionMascota.style.display = "none"
+    }    
+}
+
+
+function seleccionarMascotaEnemigo(){
+    let mascotaEnemigo = document.getElementById("mascota-enemigo")
+    let enemigos = new Map();
+    enemigos.set(1, "Hipodoge")
+    enemigos.set(2, "Capipepo")
+    enemigos.set(3, "Ratigueya")
+    let enemigoAleatorio = enemigos.get(numeroAleatorio(1,3))
+    mascotaEnemigo.innerHTML = enemigoAleatorio
 }
 
 function combate(){
@@ -78,22 +120,26 @@ function combate(){
     
 }
 
-function seleccionarAtaqueEnemigo(){
-    let ataques = new Map();
-    ataques.set(1, "FUEGO")
-    ataques.set(2, "AGUA")
-    ataques.set(3, "TIERRA")
-    ataqueEnemigo = ataques.get(numeroAleatorio(1,3))
-    combate()
-    crearMensajeCombate()
-    if (vidasJugador == 0) {
-        crearMensajeFinal()
-        deshabilitarAtaques()
-    } else if (vidasEnemigo == 0) {
-        crearMensajeFinal()
-        deshabilitarAtaques()
-    }
+
+function crearMensajeCombate(){
+    let mensajes = document.getElementById("mensajes")
+
+    let parrafo =  document.createElement("p")
+    parrafo.innerHTML = `Tu mascota atacó con ${ataqueJugador}, la mascota del enemigo atacó con ${ataqueEnemigo} - ${resultado}`
+
+    mensajes.appendChild(parrafo)
 }
+
+
+function crearMensajeFinal(){
+    let mensajes = document.getElementById("mensajes")
+
+    let parrafo =  document.createElement("p")
+    parrafo.innerHTML = ` ${vidasJugador == 0 ? "Losiento, has perdido:(" : "Felicidades has ganado"}`
+
+    mensajes.appendChild(parrafo)
+}
+
 
 function deshabilitarAtaques(){
     let botonFuego = document.getElementById("boton-fuego")
@@ -104,39 +150,10 @@ function deshabilitarAtaques(){
     botonTierra.disabled = true
 }
 
-function seleccionarMascotaJugador(){
-    let inputHipodoge = document.getElementById("hipodoge")
-    let inputCapipepo = document.getElementById("capipepo")
-    let inputRatigueya = document.getElementById("ratigueya")
-    let mascotaJugador = document.getElementById("mascota-jugador")
 
-    if (inputHipodoge.checked){
-        alert("Seleccionaste la mascota Hipodoge")
-        mascotaJugador.innerHTML = "Hipodoge"
-    } else if (inputCapipepo.checked){
-        alert("Seleccionaste la mascota Capipepo")
-        mascotaJugador.innerHTML = "Capipepo"
-    } else if (inputRatigueya.checked){
-        alert("Seleccionaste la mascota Ratigueya")
-        mascotaJugador.innerHTML = "Ratigueya"
-    } else{
-        alert("No seleccionaste ninguna mascota")
-    }
-    if (mascotaJugador.innerHTML != ""){
-        seleccionarMascotaEnemigo()
-    }    
-}
-
-
-function seleccionarMascotaEnemigo(){
-    let mascotaEnemigo = document.getElementById("mascota-enemigo")
-    let enemigos = new Map();
-    enemigos.set(1, "Hipodoge")
-    enemigos.set(2, "Capipepo")
-    enemigos.set(3, "Ratigueya")
-    let enemigoAleatorio = enemigos.get(numeroAleatorio(1,3))
-    mascotaEnemigo.innerHTML = enemigoAleatorio
-
+function habilitarReiniciar(){
+    let seccionReiniciar = document.getElementById("reiniciar")
+    seccionReiniciar.style.display = "block"
 }
 
 
@@ -144,6 +161,8 @@ function reiniciarJuego(){
     location.reload()
     
 }
+
+
 function numeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
