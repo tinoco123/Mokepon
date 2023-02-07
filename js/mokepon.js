1dc5c2552cc7a5649e486c6aec37e2ec025b3c9d
Jonathan Tinoco <tinocojona@outlook.com>
2023-02-07 01:07:20 -0600

    canvas, mover capipepo a todas las direcciones

--- js/mokepon.js
+++ js/mokepon.js
@@ -16,6 +16,7 @@ let botonesAtaque = []
 let secuenciaAtaqueJugador = []
 let secuenciaAtaqueEnemigo = []
 let ataquesMokeponEnemigo = []
+let intervalo
 
 const botonMascota = document.getElementById("boton-mascota")
 const botonReiniciar = document.getElementById("boton-reiniciar")
@@ -37,6 +38,10 @@ const seccionAtaquesEnemigo = document.getElementById("ataques-enemigo")
 const contenedorTarjetas = document.getElementById("contenedor-tarjetas")
 
 const seccionTarjetasAtaquesJugador = document.getElementById("tarjetas-ataque-jugador")
+const seccionVerMapa = document.getElementById("ver-mapa")
+const mapa = document.getElementById("mapa")
+let lienzo = mapa.getContext("2d")
+
 
 class Mokepon{
     constructor (nombre, foto, vida){
@@ -44,12 +49,20 @@ class Mokepon{
         this.foto = foto
         this.vida = vida
         this.ataques = []
+        this.x = 20
+        this.y = 30
+        this.ancho = 80
+        this.alto = 80
+        this.mapaFoto = new Image()
+        this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
-let hipodoge = new Mokepon("Hipodoge", "./assets/img/hipodoge.webp", 3)
-let capipepo = new Mokepon("Capipepo", "./assets/img/capipepo.webp", 3)
-let ratigueya = new Mokepon("Ratigueya", "./assets/img/ratigueya.webp", 3)
+let hipodoge = new Mokepon("Hipodoge", "../assets/img/hipodoge.webp", 3)
+let capipepo = new Mokepon("Capipepo", "../assets/img/capipepo.webp", 3)
+let ratigueya = new Mokepon("Ratigueya", "../assets/img/ratigueya.webp", 3)
 
 mokepones.push(hipodoge, capipepo, ratigueya)
 
@@ -97,6 +110,7 @@ function iniciarJuego(){
     botonMascota.addEventListener("click", seleccionarMascotaJugador)
     botonReiniciar.addEventListener("click", reiniciarJuego)
     // Desactivar contenido para evitar un mal flujo del juego
+    seccionVerMapa.style.display = "none"
     seccionAtaques.style.display = "none"
     seccionReiniciar.style.display = "none"
 }
@@ -116,9 +130,11 @@ function seleccionarMascotaJugador(){
         alert("No seleccionaste ninguna mascota")
     }
     if (mascotaJugadorHTML.innerHTML != ""){
-        cargarAtaques()
-        seleccionarMascotaEnemigo()
-        seccionAtaques.style.display = "flex"
+        seccionVerMapa.style.display = "flex"
+        intervalo = setInterval(pintarPersonaje, 50)
+        // seleccionarMascotaEnemigo()
+        // cargarAtaques()
+        // seccionAtaques.style.display = "flex"
         seccionMascota.style.display = "none"
     }    
 }
@@ -281,5 +297,41 @@ function numeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function pintarPersonaje() {
+    capipepo.x += capipepo.velocidadX
+    capipepo.y += capipepo.velocidadY
 
+    lienzo.clearRect(0, 0, mapa.width, mapa.height)
+    lienzo.drawImage(
+        capipepo.mapaFoto,
+        capipepo.x,
+        capipepo.y,
+        capipepo.ancho,
+        capipepo.alto
+    )
+}
+
+function moverDerecha() {
+    capipepo.velocidadX = 5
+}
+
+
+function moverIzquierda() {
+    capipepo.velocidadX = -5
+}
+
+
+function moverArriba() {
+    capipepo.velocidadY= -5
+}
+
+
+function moverAbajo() {
+    capipepo.velocidadY = 5
+}
+
+function detenerMovimiento() {
+    capipepo.velocidadX = 0
+    capipepo.velocidadY = 0
+}
 window.addEventListener("load", iniciarJuego)
